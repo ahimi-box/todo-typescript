e5364559a9024ee8882aa8077efceb97f4d993f6
ahimi <testtestabe07@gmail.com>
2025-12-12 15:23:13 +0900

    feat:add Folder and Task types

---
+++ front/src/types/folder.ts
@@ -0,0 +1,6 @@
+export type Folder = {
+  id: number;
+  title: string;
+  created_at: string;
+  updated_at: string;
+}
---
+++ front/src/types/task.ts
@@ -0,0 +1,9 @@
+export type Task = {
+  id: number;
+  folder_id: number;
+  title: string;
+  status: number;
+  due_date: string;
+  created_at: string;
+  updated_at: string;
+}
