0ae39a2fdc7fe7ae8478c6d98407dd816c66cc5d
ahimi <testtestabe07@gmail.com>
2026-01-09 11:22:03 +0900

    feat: add Home page

---
+++ front/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import FolderList from "../components/FolderList";
+import { Folder } from "../types/folder";
+
+export default function Home() {
+  const handleSelect = (folder: Folder) => {
+    console.log("選択されたフォルダ:", folder);
+  };
+
+  return (
+    <div>
+      <h1>フォルダ一覧</h1>
+      <FolderList onSelect={handleSelect} />
+    </div>
+  );
+}
